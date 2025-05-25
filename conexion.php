321899b6958c8843e2f3517fa2c012bb4bc7cc5b
ialgar367 <ialgar@elpuig.xeill.net>
2025-05-25 20:00:27 +0200

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -1,8 +1,8 @@
 <?php
-$host = "localhost";
-$usuario = "root";
-$clave = "";
-$bd = "onepiece";
+$host = "sql209.infinityfree.com";
+$usuario = "if0_39077671";
+$clave = "w7GtUDDybXqY3";
+$bd = "if0_39077671_onepiece";
 
 $conexion = new mysqli($host, $usuario, $clave, $bd);
 
